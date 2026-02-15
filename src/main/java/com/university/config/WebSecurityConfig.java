6bd0d6bb4b055bf880963749ac3a39731fc836fc
Tushar Samaniya <tusharsamaniya4956@gmail.com>
2026-02-15 13:23:36 +0530

    working with requestMatch

--- src/main/java/com/university/config/WebSecurityConfig.java
+++ src/main/java/com/university/config/WebSecurityConfig.java
@@ -1,8 +1,24 @@
 package com.university.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class WebSecurityConfig {
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
+		httpSecurity
+		.authorizeHttpRequests(auth -> auth
+				.requestMatchers("/student/**").permitAll()
+				.requestMatchers("/teacher/**").authenticated()
+		)
+		.formLogin(Customizer.withDefaults());
+		return httpSecurity.build();
+		
+	}
 
 }
